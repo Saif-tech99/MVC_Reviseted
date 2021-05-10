f19b218fbee7fa76d54361842b4a9a249aa8d807
Saif Al-adilee <saifa9697@gmail.com>
2021-05-10 11:55:16 +0200

    Add project files.

---
+++ MVC_Reviseted/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Reviseted.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult SayHello()
+        {
+            ViewBag.Hello = "Hello C-Sharper's";
+            ViewBag.Year = "2020";
+
+            ViewData["Name"] = "Saif";
+            ViewData["LastName"] = "Saif2";
+
+            return View();
+        }
+    }
+}
